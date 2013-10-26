751b2ba3988dfd12ee0fabe7c2316061cb83dd1b
Andreas Thorning <thorning@calendize.com>
2013-10-26 17:56:27 +0200

    updated tests

--- test/rhetorical.js
+++ test/rhetorical.js
@@ -1,44 +1,64 @@
 var rhetorical = require('../../rhetorical');
 
-rhetorical.init({
-  file : __dirname + '/.asserts'
-});
+
+var test_data = {
+  string1 : '1',
+  num1    : 1,
+  bool1   : true,
+  bool2   : false,
+  date1   : new Date(2000, 0, 1),
+  object1 : {
+    num2    : 2,
+    string2 : '2'
+  }
+}
 
 describe('rhetorical', function () {
 
-  var data = {
-      string1 : '1',
-      int1    : 1,
-      bool1   : true,
-      bool2   : false,
-      object1 : {
-        int2    : 2,
-        string2 : '3'
-      }
-    }
+  before(function () {
+    rhetorical.init({
+      file : __dirname + '/.asserts'
+    });
+  })
 
-  it ('should work', function () {
-    var meta_data = {
-      int1 : {
-        $exists : true  
-      },
-      object1 : {
-        string2 : {
-          $exists : true
-        }
-      }
-    };
-    rhetorical('should work', data, meta_data);
+  it ('should be equal to test data', function () {
+    rhetorical('my test data is', test_data);
+  })
+
+  it ('should work for other test_data', function () {
+    test_data.num1 = 2;
+    rhetorical('my mutated test data is', test_data);
   });
 
-  // it ('should also work', function () {
-  //   data.int1 = 2;
-  //   rhetorical('should also work', data);
-  // });
+  it ('should work as long as nums exists', function () {
+    test_data.num1 = Math.random();
+    test_data.object1.num2 = Math.random();
+    var meta_data = {
+      num1    : {$exists : true},
+      object1 : {
+        num2 : {$exists : true}
+      } 
+    }
+    rhetorical('my random test data is', test_data, meta_data);
+  });
 
-  // it ('should not work', function () {
-  //   data.int1 = 2;
-  //   rhetorical('should work', data);
-  // });
+  it ('should work even though i run it later', function () {
+    test_data.date1 = new Date();
 
+    test_data.num1 = 1;
+    test_data.object1.num2 = 2;
+
+    var meta_data = {
+      date1 : {$exists : true}
+    }
+    rhetorical('my testdata today is', test_data, meta_data);
+  });
+
+  it ('should NOT work', function () {
+    test_data.string1 = 'something new';
+
+    //I ask the same question as in the first test, and expect
+    //it to fail, as the data has changed
+    rhetorical('my test data is', test_data);
+  });
 })
